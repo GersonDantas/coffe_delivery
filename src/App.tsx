aad3ed34dfc24b5930e76ab49be6a338a433936c
gersondantas <53773325+GersonDantas@users.noreply.github.com>
2024-11-14 20:57:22 -0300

    chore: init config to project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme'
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
